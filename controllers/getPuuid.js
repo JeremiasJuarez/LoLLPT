c96b675326570a6472e39f34eb3249546ee5e68a
Jeremias <jeremiasudemy1@gmail.com>
2024-11-18 12:59:55 -0300

    getpuuid error act

--- controllers/getPuuid.js
+++ controllers/getPuuid.js
@@ -23,7 +23,7 @@ const getPuuid = async( req, res = response ) => {
     
     } catch (error) {
         res.status(404).json({
-            error: error.message,
+            riotError: error.message,
             ok: false,
             msg: 'Summoner not found - invalid or non-existent gameName#tagLine',
         });
