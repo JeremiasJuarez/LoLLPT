da32d39f6057d269255ff51f793f826177de0101
Jeremias <jeremiasudemy1@gmail.com>
2024-11-29 16:36:59 -0300

    getRankProfile updated for multiple server searchs

--- controllers/getRankProfile.js
+++ controllers/getRankProfile.js
@@ -3,37 +3,31 @@ const { response } = require('express')
 
 const getRankProfile = async( req, res = response ) => {
 
-    const { summonerId } = req.query
+    const { summonerId, server } = req.query
     
+    console.log( summonerId )
+    console.log( server )
+
     try {
-        
-        const url = `https://la2.api.riotgames.com/lol/league/v4/entries/by-summoner/${ summonerId }?api_key=${ process.env.RGAPI }`
+        const url = `https://${ server }.api.riotgames.com/lol/league/v4/entries/by-summoner/${ summonerId }?api_key=${ process.env.RGAPI }`
         const data = await fetch( url )
         const rankProfile = await data.json()
         
-        if( rankProfile.length === 0 ){
-            return res.json({
-                rankProfile: [],
-                msg: 'Summoner exists but has not played any ranked games recently',
-                ok: true,
-            });
-        }
+       if( !data.ok ){
+        throw new Error('Error at fetching RIOT endpoint')
+       }
 
-        if (data.status === 400) {
-            throw new Error(rankProfile.status?.message || 'Bad Request: Invalid data');
-        }
-
-        res.json({
-            rankProfile: rankProfile,
-            msg: 'rankProfile obtained',
-            ok: true,
-        })
+       res.json({
+        rankProfile: rankProfile,
+        ok: true,
+        msg: 'Rankprofile Obtained'
+       })
 
     } catch (error) {
         res.status(404).json({
             riotError: error.message,
             ok: false,
-            msg: 'summonerId not found - Invalid or non-existent summonerId',
+            msg: 'Rankprofile not found - Invalid or non-existent summonerId',
         });
     }
 
@@ -41,3 +35,5 @@ const getRankProfile = async( req, res = response ) => {
 }
 
 module.exports = getRankProfile
+
+//*U4ZlPbscYwuOUMIqSYcYjA7hEU5gJcx0l1f1guraEDtJuyU
